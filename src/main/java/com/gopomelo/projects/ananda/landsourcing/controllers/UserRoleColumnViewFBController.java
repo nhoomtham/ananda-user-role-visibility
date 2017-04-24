729bdc280543d923460b865b2f11bfcc4611cb28
NK <natthaphol@gopomelo.com>
2017-04-24 18:07:06 +0700

    modify using firebase

--- src/main/java/com/gopomelo/projects/ananda/landsourcing/controllers/UserRoleColumnViewFBController.java
+++ src/main/java/com/gopomelo/projects/ananda/landsourcing/controllers/UserRoleColumnViewFBController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.client.RestTemplate;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.gopomelo.projects.ananda.landsourcing.controllers.vo.UserRoleVO;
 
@@ -21,7 +23,7 @@ import com.gopomelo.projects.ananda.landsourcing.controllers.vo.UserRoleVO;
 @Controller
 public class UserRoleColumnViewFBController {
 
-	private Logger LOG = Logger.getLogger(UserRoleColumnViewFBController.class);
+	private static final Logger LOG = Logger.getLogger(UserRoleColumnViewFBController.class);
 
 	@Autowired
 	private DatabaseReference firebaseDatabse;
@@ -32,17 +34,83 @@ public class UserRoleColumnViewFBController {
 
 		try {
 
-			final String uri = "http://localhost:8080/springrestexample/employees.json";
+			// final String uri = "https://gcp-labs-nk.firebaseio.com/";
+			//
+			// RestTemplate restTemplate = new RestTemplate();
+			// String result = restTemplate.getForObject(uri, String.class);
 
-			RestTemplate restTemplate = new RestTemplate();
-			String result = restTemplate.getForObject(uri, String.class);
+			// LOG.info(result);
 
-			System.out.println(result);
+			firebaseDatabse.child("k1").addChildEventListener(new ChildEventListener() {
+
+				@Override
+				public void onCancelled(DatabaseError databaseError) {
+					// ...
+				}
+
+				@Override
+				public void onChildAdded(DataSnapshot dataSnapshot, String arg1) {
+					// TODO Auto-generated method stub
+					LOG.info(
+							"UserRoleColumnViewFBController.findRoleColumnView(...).new ChildEventListener() {...}.onChildAdded()");
+					LOG.info("+++++++++childCount[" + dataSnapshot.getChildrenCount() + "]+++++++++[" + arg1);
+
+					Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+					LOG.info("+++++++++dataSnapshot.getChildren[" + children + "]+++++++++[" + arg1);
+					for (DataSnapshot child : children) {
+						LOG.info("--- child[" + child.getValue() + "]---");
+					}
+				}
+
+				@Override
+				public void onChildChanged(DataSnapshot dataSnapshot, String arg1) {
+					// TODO Auto-generated method stub
+					LOG.info(
+							"UserRoleColumnViewFBController.findRoleColumnView(...).new ChildEventListener() {...}.onChildChanged()");
+					LOG.info("+++++++++childCount[" + dataSnapshot.getChildrenCount() + "]+++++++++[" + arg1);
+
+					Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+					LOG.info("+++++++++dataSnapshot.getChildren[" + children + "]+++++++++[" + arg1);
+					for (DataSnapshot child : children) {
+						LOG.info("--- child[" + child.getValue() + "]---");
+					}
+
+				}
+
+				@Override
+				public void onChildMoved(DataSnapshot dataSnapshot, String arg1) {
+					// TODO Auto-generated method stub
+					LOG.info(
+							"UserRoleColumnViewFBController.findRoleColumnView(...).new ChildEventListener() {...}.onChildMoved()");
+					Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+
+					LOG.info("+++++++++dataSnapshot.getChildren[" + children + "]+++++++++[" + arg1);
+					for (DataSnapshot child : children) {
+						LOG.info("--- child[" + child.getValue() + "]---");
+					}
+
+				}
+
+				@Override
+				public void onChildRemoved(DataSnapshot dataSnapshot) {
+					// TODO Auto-generated method stub
+					LOG.info(
+							"UserRoleColumnViewFBController.findRoleColumnView(...).new ChildEventListener() {...}.onChildRemoved()");
+
+					Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+					LOG.info("+++++++++dataSnapshot.getChildren[" + children + "]+++++++++");
+					for (DataSnapshot child : children) {
+						LOG.info("--- child[" + child.getValue() + "]---");
+					}
+
+				}
+			});
 
 		} catch (Exception ex) {
 			// TODO
+			LOG.trace(ex.getMessage(), ex);
 		}
-		return result;
+		return "";
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/userrolesfb/")
