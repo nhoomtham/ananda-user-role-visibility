aaba1305913899463a03eee5e7d2534e52c06b9f
NK <natthaphol@gopomelo.com>
2017-04-27 16:55:51 +0700

    update to add flow stage

--- data/config-patch-form-fields.sh
+++ data/config-patch-form-fields.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#export PROJ_NAME=gcp-labs-nk
+# export PROJ_NAME=gcp-labs-nk
 export PROJ_NAME=ananda-cloud
 export FB_URL="https://${PROJ_NAME}.firebaseio.com"
 export ROLE_PATH="rest/saving-data/user_role"
